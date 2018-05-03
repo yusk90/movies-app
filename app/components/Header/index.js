09b745ae988a3cc94a23b18e38396167477f98f0
Yurii Skorniakov <yurii.skorniakov@gmail.com>
2018-05-03 22:21:03 +0300

    Add movie details component

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './index.less';
 
-const Header = () => (
+const Header = ({ children }) => (
   <div className="header">
-    <div className="container">movieroulette</div>
+    <div className="container">
+      movieroulette
+      {children}
+    </div>
   </div>
 );
+
+Header.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+};
 
 export default Header;
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
-import { Header, Footer } from 'components';
+import { Header, Footer, MovieDetails } from 'components';
 import './index.less';
+
+const movie = {
+  title: 'The Godfather',
+  rating: 9.2,
+  posterSrc: 'https://ia.media-imdb.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,704,1000_AL_.jpg',
+  genre: 'Crime, Drama',
+  year: 1972,
+  length: '2h 55min',
+  description: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
+};
 
 const App = () => (
   <div className="app">
     <div className="app__header">
-      <Header />
+      <Header>
+        <MovieDetails {...movie} />
+      </Header>
     </div>
     <div className="app__footer">
       <Footer />
