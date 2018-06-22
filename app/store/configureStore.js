cd052ea64492932f2e022cfb7122174d1fed010f
yusk90 <yurii.skorniakov@gmail.com>
2018-06-22 17:26:00 +0300

    Add saving state to localStorage

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,15 +1,24 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import * as storage from 'redux-storage';
+import createEngine from 'redux-storage-engine-localstorage';
 
 import rootReducer from '../reducers';
 import api from '../middleware/api';
 
+const engine = createEngine('movies');
+const reducer = storage.reducer(rootReducer);
+const engineMiddleware = storage.createMiddleware(engine);
+
 const configureStore = preloadedState => {
   const store = createStore(
-    rootReducer,
+    reducer,
     preloadedState,
-    compose(applyMiddleware(thunk, api))
+    compose(applyMiddleware(api, thunk, engineMiddleware))
   );
+  const load = storage.createLoader(engine);
+
+  load(store);
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
