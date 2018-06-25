5783378bb1c8d2e0c4b651df6d56bc51f632f59b
Yurii Skorniakov <yurii.skorniakov@gmail.com>
2018-06-25 21:12:57 +0300

    Add Logo component

--- app/components/index.js
+++ app/components/index.js
@@ -1,5 +1,6 @@
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
+export { default as Logo } from './Logo';
 export { default as MovieDetails } from './MovieDetails';
 export { default as MoviePreview } from './MoviePreview';
 export { default as MoviePreviewList } from './MoviePreviewList';
